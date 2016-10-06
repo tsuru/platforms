7374364108717e6e11dbb7699733b8b05419c9d6
Raphael Amorim <rapha850@gmail.com>
2016-10-06 15:27:51 -0300

    update copyright

--- php/frontend.py
+++ php/frontend.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 tsuru authors. All rights reserved.
+# Copyright 2016 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
